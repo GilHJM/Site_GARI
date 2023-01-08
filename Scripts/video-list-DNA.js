8950430adbcc73d04c72804195ac6010b4b4c74c
GilHJM <111406866+GilHJM@users.noreply.github.com>
2023-01-07 23:35:54 -0300

    0006A
    DNA-update

--- Scripts/video-list-DNA.js
+++ Scripts/video-list-DNA.js
@@ -34,41 +34,45 @@ let allVideos = [
       src: "https://drive.google.com/file/d/1wOMXnt9oo4vvq57vLOz06OmTThTl8Xg7/preview", 
       id: "vid_7"
    },
-
    {
-      name: " 27/09/2022",
-      src: "https://drive.google.com/file/d/1wOMXnt9oo4vvq57vLOz06OmTThTl8Xg7/preview", 
+      name: "04/10/2022Part1",
+      src: "https://drive.google.com/file/13Zazxkg7TkDjH6VzR5qscPqdjnrzr531/preview",
       id: "vid_8"
-   },
-   {
-      name: " 27/09/2022",
-      src: "https://drive.google.com/file/d/1wOMXnt9oo4vvq57vLOz06OmTThTl8Xg7/preview", 
+      },
+      {
+      name: "04/10/2022Part2",
+      src: "https://drive.google.com/file/1mWcU1fD0pjf0wSydWoxw02bkubZVO8nG/preview",
       id: "vid_9"
-   },
-   {
-      name: " 27/09/2022",
-      src: "https://drive.google.com/file/d/1wOMXnt9oo4vvq57vLOz06OmTThTl8Xg7/preview", 
+      },
+      {
+      name: "11/10/2022",
+      src: "https://drive.google.com/file/1bFOnhFNuuKbz8n7jcYRs63AQaz0ZaPpP/preview",
       id: "vid_10"
-   },
-   {
-      name: " 27/09/2022",
-      src: "https://drive.google.com/file/d/1wOMXnt9oo4vvq57vLOz06OmTThTl8Xg7/preview", 
+      },
+      {
+      name: "25/10/2022",
+      src: "https://drive.google.com/file/1yg8mAjI6gb-sl8sGeImxsL8Y4sZRaVlR/preview",
       id: "vid_11"
-   },
-   {
-      name: " 27/09/2022",
-      src: "https://drive.google.com/file/d/1wOMXnt9oo4vvq57vLOz06OmTThTl8Xg7/preview", 
+      },
+      {
+      name: "01/11/2022",
+      src: "https://drive.google.com/file/1GYX1kdAomO0aeJ05faC_ss1tbJJUjLdQ/preview",
       id: "vid_12"
-   },
-   {
-      name: " 27/09/2022",
-      src: "https://drive.google.com/file/d/1wOMXnt9oo4vvq57vLOz06OmTThTl8Xg7/preview", 
+      },
+      {
+      name: "08/11/2022",
+      src: "https://drive.google.com/file/1XTy_8vKSi1oegK8j0XJkqE9Wm6UvPpr1/preview",
       id: "vid_13"
-   },
-   {
-      name: " 27/09/2022",
-      src: "https://drive.google.com/file/d/1wOMXnt9oo4vvq57vLOz06OmTThTl8Xg7/preview", 
+      },
+      {
+      name: "22/11/2022",
+      src: "https://drive.google.com/file/1srhdxrVJQnYFK41gsCjqBOw1cf82cWR3/preview",
       id: "vid_14"
-   },
+      },
+      {
+      name: "29/11/2022",
+      src: "https://drive.google.com/file/1bQwO9JztBtxfdxLqa8f1LpJk-mrFzwnE/preview",
+      id: "vid_15"
+      }
 
  ]
